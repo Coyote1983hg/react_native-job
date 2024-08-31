d2baf0e6ecf1af5e2f7db9c7e2ac974477b79d3d
Coyote1983hg <153317330+Coyote1983hg@users.noreply.github.com>
2024-08-31 19:06:47 +0200

    second commit

--- app/index.js
+++ app/index.js
@@ -1,9 +1,37 @@
-import { View,Text } from "react-native";
-const Home = () => {
-    return (
-        <View>
-         <Text>Home</Text>   
-        </View>
-    )
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World!</Text>
+        <Text style={styles.subtitle}>WawBaws wish you   $$$$$$!  Have a nice    weekend! </Text>
+      </View>
+    </View>
+  );
 }
-export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"cyan",
+    flex: 2,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+    color:"red",
+    borderColor:"black"
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "black",
+  },
+});
